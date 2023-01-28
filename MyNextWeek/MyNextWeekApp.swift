60d49a062344b99183e203fc73432450ef3701c9
Tony Short <tshort15@ford.com>
2023-01-28 17:37:33 +0000

    Initial Commit

---
+++ MyNextWeek/MyNextWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNextWeekApp.swift
+//  MyNextWeek
+//
+//  Created by Tony Short on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyNextWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
